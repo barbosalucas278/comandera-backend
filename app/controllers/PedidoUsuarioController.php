e33bdb8bc46b2bdae8e131878cfae1363ae600c6
barbosalucas278 <barbosalucasroberto@gmail.com>
2021-05-24 18:20:33 -0300

    intento 4 de arreglar el path de PedidoUsuario.php

--- app/controllers/PedidoUsuarioController.php
+++ app/controllers/PedidoUsuarioController.php
@@ -3,7 +3,6 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-require_once './models/PedidoUsuario.php';
 class PedidoUsuarioController
 {
     public function OperacionesPorSector(Request $request, Response $response, array $args)
