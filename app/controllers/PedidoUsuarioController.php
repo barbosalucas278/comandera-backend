87cb5dc98813ab8106f7f05a3a72d124b54e1b51
barbosalucas278 <barbosalucasroberto@gmail.com>
2021-05-24 18:39:55 -0300

    intento 6 de arreglar el path de PedidoUsuario.php

--- app/controllers/PedidoUsuarioController.php
+++ app/controllers/PedidoUsuarioController.php
@@ -3,6 +3,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+require_once './models/PedidoUsuario.php';
 class PedidoUsuarioController
 {
     public function OperacionesPorSector(Request $request, Response $response, array $args)
--- app/index.php
+++ app/index.php
@@ -16,7 +16,6 @@ require_once './db/AccesoDatos.php';
 require_once './middlewares/MWAutenticar.php';
 require_once './middlewares/MWAccesos.php';
 require_once './middlewares/MWLogger.php';
-require_once './models/PedidoUsuario.php';
 
 require_once './controllers/UsuarioController.php';
 require_once './controllers/MesaController.php';
