72f2ca613ab7d6314e1285ae5c9fe307a4f2b64b
barbosalucas278 <barbosalucasroberto@gmail.com>
2021-06-07 16:41:46 -0300

    fix path

--- app/controllers/PedidosController.php
+++ app/controllers/PedidosController.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require_once './models/Pedido.php';
-require_once './models/PedidoUsuario.php';
+require_once './models/Pedidousuario.php';
 require_once './interfaces/IApiUsable.php';
 class PedidosController implements IApiUsable
 {
